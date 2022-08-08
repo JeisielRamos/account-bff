0825747397005e0d226e1038a1474dfd6fd02f2a
jeisiel.oliveira <jeisiel.oliveira@b2wdigital.com>
2022-08-08 20:12:33 -0300

    health-check-and-init-configs

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,32 @@
 package main
+
+import (
+	"os"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
+)
+
+func healthCheck(ctx *fiber.Ctx) error {
+	return ctx.Status(fiber.StatusOK).JSON(&fiber.Map{
+		"status": "success",
+	})
+}
+
+func main() {
+	godotenv.Load()
+
+	app := fiber.New()
+
+	app.Get("/helthCheck", healthCheck)
+
+	httpPort := os.Getenv("HTTP_PORT")
+	if httpPort == "" {
+		httpPort = "5000"
+	}
+
+	err := app.Listen(":" + httpPort)
+	if err != nil {
+		panic(err)
+	}
+}
